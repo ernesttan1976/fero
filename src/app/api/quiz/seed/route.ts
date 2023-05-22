6047753750aee166c850041f6b6eb3c59ca1fa30
Eileenpngg <eileenpng1843@gmail.com>
2023-05-22 18:50:37 +0800

    set up models and seed

---
+++ src/app/api/quiz/seed/route.ts
@@ -0,0 +1,6 @@
+import { NextResponse } from "next/server";
+import seedQuestions from "@/app/lib/seedQuestions";
+export async function GET() {
+  await seedQuestions();
+  return NextResponse.json({ message: "Seed Questions Successful" });
+}
--- src/app/api/users/seed/route.ts
+++ src/app/api/users/seed/route.ts
@@ -2,6 +2,6 @@ import { NextResponse } from "next/server";
 import seedUsers from "@/app/lib/seedUsers";
 //SEED
 export async function GET() {
-    seedUsers();
-    return NextResponse.json({"message": "Seed Users Successful"});
+  seedUsers();
+  return NextResponse.json({ message: "Seed Users Successful" });
 }
